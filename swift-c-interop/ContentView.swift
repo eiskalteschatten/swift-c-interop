5502d2d810823eb3a5a70bf4a16b51708c6e3f43
Alex Seifert <alex@alexseifert.com>
2025-02-25 08:19:31 +0100

    Initial Commit

---
+++ swift-c-interop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-c-interop
+//
+//  Created by Benjamin Seifert on 25.02.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-c-interop/swift_c_interopApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_c_interopApp.swift
+//  swift-c-interop
+//
+//  Created by Benjamin Seifert on 25.02.25.
+//
+
+import SwiftUI
+
+@main
+struct swift_c_interopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
